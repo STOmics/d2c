539bc0cdf95062bf7f786d84840354b21c6ec81e
fxzhao <zhaofuxiang@genomics.cn>
2022-12-15 09:59:11 +0800

    release v1.4.5

--- d2c/main.cpp
+++ d2c/main.cpp
@@ -32,7 +32,7 @@ namespace fs = std::filesystem;
 
 //#define DEVEL
 constexpr auto APP_NAME    = "D2C";
-constexpr auto APP_VERSION = "1.4.4";
+constexpr auto APP_VERSION = "1.4.5";
 
 int main(int argc, char** argv)
 {
